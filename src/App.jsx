7432e3e5826a09f8d836c350631d744edbdf1fe6
Francky <contact@francky.pl>
2024-04-16 00:12:05 +0200

    create generator

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,53 @@ import "./App.css";
 function App() {
   const [list, setList] = useState([]);
 
+  function generateNumber() {
+    return Math.floor(Math.random() * (90 - 1 + 1) + 1);
+  }
+
+  function isNumInList(n) {
+    return list.includes(n);
+  }
+
+  function handleGenerateClick() {
+    const n = generateNumber();
+    if (!isNumInList(n)) {
+      setTimeout(() => setList([...list, n]), 1000);
+    }
+  }
+
   return (
     <>
       <h1>Numeri</h1>
-      <ul>{list.length > 0 && <li>{list}</li>}</ul>
+      <ul
+        style={{
+          display: "flex",
+          gap: "1rem",
+          listStyle: "none",
+          height: "50px",
+        }}
+      >
+        {list.length > 0 &&
+          list.map((n) => (
+            <li
+              key={n}
+              style={{
+                width: "50px",
+                height: "50px",
+                lineHeight: "50px",
+                background: "white",
+                color: "green",
+                borderRadius: "55px",
+                fontSize: "2rem",
+                fontWeight: "bold",
+              }}
+            >
+              {n}
+            </li>
+          ))}
+      </ul>
+      {list.length < 6 && <button onClick={handleGenerateClick}>Genera</button>}
+      {list.length > 5 && <button onClick={() => setList([])}>Resetta</button>}
     </>
   );
 }
